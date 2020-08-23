272539a6b305fe3a94cb3b1f08b18b118f5b6c94
Raees-Ahamed <49068534+Raees-Ahamed@users.noreply.github.com>
2020-08-22 23:53:21 -0700

    Updated - Added Email Field and Password Field
    Added email and password field in login view

--- NIBM COVID19/NIBM COVID19/View/LoginView.swift
+++ NIBM COVID19/NIBM COVID19/View/LoginView.swift
@@ -10,36 +10,123 @@ import UIKit
 
 class LoginView: UIViewController {
     
-    private var titleLabel: UILabel = {
-       let label = UILabel()
-        label.text = "NIBM-Covid"
-        label.font = UIFont(name:"Avenir-Light", size: 36)
+    //Mark :- Properties
+    
+    
+    
+    
+   private lazy var emailContainerView: UIView = {
+        let view = UIView()
+        
+        let imageView = UIImageView()
+        //imageView.image = #imageLiteral(resourceName: "ic_mail_outline_white_2x")
+        imageView.alpha = 0.87
+        view.addSubview(imageView)
+        imageView.centerY(inView: view)
+        imageView.anchor(left: view.leftAnchor, paddingLeft: 8, width: 24, height: 24)
+        
+        view.addSubview(emailTextFiled)
+        emailTextFiled.centerY(inView: view)
+        emailTextFiled.anchor(left: imageView.rightAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingLeft: 8, paddingBottom: 8)
+        
+        let separatorView = UIView()
+        separatorView.backgroundColor = .lightGray
+        view.addSubview(separatorView)
+        separatorView.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingLeft: 8, height: 0.75)
+        
+        return view
+    }()
+    
+    private lazy var passwordContainerField : UIView = {
+       
+        let view = UIView()
+        
+        let imageView = UIImageView()
+               //imageView.image = #imageLiteral(resourceName: "ic_mail_outline_white_2x")
+        imageView.alpha = 0.87
+        view.addSubview(imageView)
+        imageView.centerY(inView: view)
+        imageView.anchor(left: view.leftAnchor, paddingLeft: 8, width: 24, height: 24)
+               
+        
+        view.addSubview(passwordTextField)
+        passwordTextField.centerY(inView: view)
+        passwordTextField.anchor(left: imageView.rightAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingLeft: 8, paddingBottom: 8)
+        
+        let separatorView = UIView()
+        separatorView.backgroundColor = .lightGray
+        view.addSubview(separatorView)
+        separatorView.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingLeft: 8, height: 0.75)
+        
+        return view
+        
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "NTAXI"
+        label.font = UIFont(name: "Avenir-Light", size: 36)
+        label.textColor = UIColor(white: 1, alpha: 0.8)
         
         return label
     }()
     
-    private var userNameText : UIView = {
-       let text = UITextView()
-        text.text = "Hello"
-        text.font = UIFont(name:"Avenir-Light", size: 36)
+    private let emailTextFiled: UIView = {
+        let tf = UITextField()
+        tf.borderStyle = .none
+        tf.font = UIFont.systemFont(ofSize: 16)
+        tf.textColor = .white
+        tf.keyboardAppearance = .dark
+        tf.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
         
-        return text
-        
+        return tf
     }()
+    
+    private let passwordTextField: UIView = {
+        let tf = UITextField()
+        tf.borderStyle = .none
+        tf.font = UIFont.systemFont(ofSize: 16)
+        tf.textColor = .white
+        tf.keyboardAppearance = .dark
+        tf.attributedPlaceholder = NSAttributedString(string: "password", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
+        
+        return tf
+    }()
+    
+    
+    private lazy var submitButtonContainer : UIView = {
+       let view = UIView()
+       
+        return view
+    }()
+    
+    
+    
+    
+    
+    //Mark :- Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .red
+        //view.backgroundColor = .red
+        
         
         view.addSubview(titleLabel)
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+             titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor)
+             titleLabel.centerX(inView: view)
+        
+        view.addSubview(emailContainerView)
+        emailContainerView.anchor(top: titleLabel.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 40, paddingLeft: 16, paddingRight: 16, height: 50)
+        
+        view.addSubview(passwordContainerField)
+        passwordContainerField.anchor(top: emailContainerView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 40, paddingLeft: 16, paddingRight: 16, height: 50)
+
         
         
+        
+        }
     }
-    
 
     /*
     // MARK: - Navigation
@@ -51,4 +138,4 @@ class LoginView: UIViewController {
     }
     */
 
-}
+
