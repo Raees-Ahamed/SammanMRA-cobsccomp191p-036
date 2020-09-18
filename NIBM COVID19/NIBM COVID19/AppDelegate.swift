aa453657b53c3e7ab05336d5a5300f805b0b91e1
Raees-Ahamed <49068534+Raees-Ahamed@users.noreply.github.com>
2020-09-18 12:41:40 -0700

    FIxed UI errors

--- NIBM COVID19/NIBM COVID19/AppDelegate.swift
+++ NIBM COVID19/NIBM COVID19/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         FirebaseApp.configure()
         window = UIWindow()
         window?.makeKeyAndVisible()
-        window?.rootViewController = UINavigationController(rootViewController: ProfileViewController())
+        window?.rootViewController = UINavigationController(rootViewController: SurveyViewController())
         return true
     }
 }
